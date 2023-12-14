569fd062f1b0ee8aaa35083f315bc35d2e3a9250
WangshuXC <1294673511@qq.com>
2023-12-14 18:08:52 +0800

    Update Flask and search_detail.html
    完成了全部高级搜索

--- flask/run.py
+++ flask/run.py
@@ -43,9 +43,9 @@ def generate_query(q):
     }
 
 
-def generate_custom_query(q1, q2, q3):
+def generate_custom_query(q1, q2, q3, q2t, q2a, q2b):
     query = {
-        "query": {"bool": {"must": [], "must_not": []}},
+        "query": {"bool": {"must": [], "must_not": [], "should": []}},
         "highlight": {
             "pre_tags": ["<strong>"],
             "post_tags": ["</strong>"],
@@ -54,31 +54,64 @@ def generate_custom_query(q1, q2, q3):
     }
 
     if q1:
-        query["query"]["bool"]["must"].append(
-            {"match": {"body": {"query": q1, "operator": "or"}}}
-        )
-        query["query"]["bool"]["must"].append(
-            {"match": {"title": {"query": q1, "operator": "or"}}}
-        )
-        query["query"]["bool"]["must"].append(
-            {"match": {"auth": {"query": q1, "operator": "or"}}}
-        )
+        keywords = q1.split()
+
+        for keyword in keywords:
+            query["query"]["bool"]["should"].extend(
+                [
+                    {"match_phrase": {"body": keyword}},
+                    {"match_phrase": {"title": keyword}},
+                    {"match_phrase": {"auth": keyword}},
+                ]
+            )
 
     if q2:
-        query["query"]["bool"]["must"].append({"match_phrase": {"body": q2}})
-        query["query"]["bool"]["must"].append({"match_phrase": {"title": q2}})
-        query["query"]["bool"]["must"].append({"match_phrase": {"auth": q2}})
+        keywords = q2.split()
+        if q2t == "true" or q2a == "true" or q2b == "true":
+            pass
+        else:
+            for keyword in keywords:
+                query["query"]["bool"]["must"].extend(
+                    [
+                        {"match_phrase": {"body": q2}},
+                    ]
+                )
+
+        if q2b == "true":
+            for keyword in keywords:
+                query["query"]["bool"]["must"].extend(
+                    [
+                        {"match_phrase": {"body": q2}},
+                    ]
+                )
+
+        if q2a == "ture":
+            for keyword in keywords:
+                query["query"]["bool"]["must"].extend(
+                    [
+                        {"match_phrase": {"auth": q2}},
+                    ]
+                )
+
+        if q2t == "ture":
+            for keyword in keywords:
+                query["query"]["bool"]["must"].extend(
+                    [
+                        {"match_phrase": {"title": q2}},
+                    ]
+                )
 
     if q3:
-        query["query"]["bool"]["must_not"].append(
-            {"match": {"body": {"query": q3, "operator": "or"}}}
-        )
-        query["query"]["bool"]["must_not"].append(
-            {"match": {"title": {"query": q3, "operator": "or"}}}
-        )
-        query["query"]["bool"]["must_not"].append(
-            {"match": {"auth": {"query": q3, "operator": "or"}}}
-        )
+        keywords = q3.split()
+
+        for keyword in keywords:
+            query["query"]["bool"]["must_not"].extend(
+                [
+                    {"match_phrase": {"body": q3}},
+                    {"match_phrase": {"auth": q3}},
+                    {"match_phrase": {"title": q3}},
+                ]
+            )
 
     return query
 
@@ -125,47 +158,23 @@ def user():
 def search():
     q = request.args.get("q", "")
     q1 = request.args.get("q1", "")
-    q2 = request.args.get("q2", "")
-    q3 = request.args.get("q3", "")
-    if q:
-        now = datetime.datetime.now()
-        timestamp = now.strftime("[%d/%b/%Y %H:%M:%S]")
-        user_ip = request.remote_addr
-        print(f"{user_ip} - - {timestamp} [普通搜索]了 ‘{q}’ -")
-        query = {
-            "query": {
-                "bool": {
-                    "should": [
-                        {
-                            "multi_match": {
-                                "query": q,
-                                "fields": [
-                                    "body",
-                                    "title",
-                                    "auth",
-                                ],
-                            }
-                        },
-                        {
-                            "bool": {
-                                "should": [
-                                    {"match_phrase": {"body": q}},
-                                    {"match_phrase": {"title": q}},
-                                    {"match_phrase": {"auth": q}},
-                                ]
-                            }
-                        },
-                    ]
-                }
-            },
-            "highlight": {
-                "pre_tags": ["<strong>"],
-                "post_tags": ["</strong>"],
-                "fields": {"body": {}, "title": {}, "auth": {}, "appreciation": {}},
-            },
-        }
 
-        data = []
+    q2 = request.args.get("q2", "")
+    q2t = request.args.get("q2t", "")
+    q2a = request.args.get("q2a", "")
+    q2b = request.args.get("q2b", "")
+
+    q3 = request.args.get("q3", "")
+
+    now = datetime.datetime.now()
+    timestamp = now.strftime("[%d/%b/%Y %H:%M:%S]")
+    user_ip = request.remote_addr
+
+    data = []
+
+    if q and not (q1 or q2 or q3):
+        print(f"{user_ip} - - {timestamp} [普通搜索]了 ‘{q}’ -")
+        query = generate_query(q)
 
         # 执行查询
         result = es.search(index="data", body=query)
@@ -193,6 +202,45 @@ def search():
             )
 
         return render_template("search_detail.html", q=q, data=data)
+    elif q1 or q2 or q3:
+        print(f"{user_ip} - - {timestamp} [高级搜索]了 ‘{q1}’, ‘{q2}’, ‘{q3}’ -")
+        query = generate_custom_query(q1, q2, q3, q2t, q2a, q2b)
+        print(query)
+        # 执行查询
+        result = es.search(index="data", body=query)
+
+        # 处理查询结果
+        for hit in result["hits"]["hits"]:
+            highlight = hit.get("highlight", {})
+            body_highlight = highlight.get("body", [])
+            title_highlight = highlight.get("title", [])
+            auth_highlight = highlight.get("auth", [])
+            appreciation_highlight = highlight.get("appreciation", [])
+
+            data.append(
+                {
+                    "title": hit["_source"]["title"],
+                    "url": hit["_source"]["url"],
+                    "content": hit["_source"]["body"],
+                    "dynasty": hit["_source"]["dynasty"],
+                    "auth": hit["_source"]["auth"],
+                    "body_highlight": body_highlight,
+                    "title_highlight": title_highlight,
+                    "auth_highlight": auth_highlight,
+                    "appreciation_highlight": appreciation_highlight,
+                }
+            )
+        print(q2t, q2a, q2b)
+        return render_template(
+            "search_detail.html",
+            q1=q1,
+            q2=q2,
+            q2t=q2t,
+            q2a=q2a,
+            q2b=q2b,
+            q3=q3,
+            data=data,
+        )
     else:
         return render_template("search.html")
 
